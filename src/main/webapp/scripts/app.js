2b11fedf6796e9427d5a625f4f0dfe0605cadf7a
mart-dominguez <mdomingu@gmail.com>
2016-05-06 13:40:28 -0300

    agrego bearer

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,9 +1,4 @@
-angular.module('relInfra', ['ngComponentRouter','angular-md5'])
-        .value('urlBase', 'http://localhost:8080/relevamientos-infraestructura/app/')
-        .value('$routerRootComponent', 'app')
-        .config(['$locationProvider',function($locationProvider) {
-            $locationProvider.html5Mode(true);
-        }])
+angular.module('relInfra', ['ngComponentRouter','angular-md5'])        
         .run(function () {
             var mdlUpgradeDom = false;
             setInterval(function() {
@@ -23,12 +18,39 @@ angular.module('relInfra', ['ngComponentRouter','angular-md5'])
         });
 
 angular.module('relInfra')
-.component('app', {
-    templateUrl:'components/app.html',
-    $routeConfig: [
-        {path: '/login', name: 'Login', component: 'login', useAsDefault: true},
-        {path: '/cuadro1', name: 'CuadroA', component: 'cuadroA'}
-    ]
+        .factory('BearerAuthInterceptor', function ($window, $q) {
+    return {
+        request: function(config) {
+            config.headers = config.headers || {};
+            if ($window.localStorage.getItem('token')) {
+              // may also use sessionStorage
+                config.headers.Authorization = 'Bearer ' + $window.localStorage.getItem('token');
+            }
+            return config || $q.when(config);
+        },
+        response: function(response) {
+            if (response.status === 401) {
+                //  Redirect user to login page / signup Page.
+            }
+            return response || $q.when(response);
+        }
+    };
 });
+angular.module('relInfra')
+        .value('urlBase', 'http://localhost:8080/relevamientos-infraestructura/app/')
+        .value('$routerRootComponent', 'app')
+        .config(function ($httpProvider) {
+                $httpProvider.interceptors.push('BearerAuthInterceptor');
+        })
+        .config(['$locationProvider',function($locationProvider) {
+            $locationProvider.html5Mode(true);
+        }])
+        .component('app', {
+            templateUrl:'components/app.html',
+            $routeConfig: [
+                {path: '/login', name: 'Login', component: 'login', useAsDefault: true},
+                {path: '/cuadro1', name: 'CuadroA', component: 'cuadroA'}
+            ]
+        });
 
 
